09516fc8e0396823488debf973e4f72c028664ab
tensei <davuttensei@gmail.com>
2016-02-12 06:01:21 +0100

    more debug code, little design change

--- Manga checker (WPF)/Handlers/DebugText.cs
+++ Manga checker (WPF)/Handlers/DebugText.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 using Manga_checker.Properties;
 
 namespace Manga_checker.Handlers {
     public class DebugText {
-        public static void Write(string text) {
+        public static void Write(string text, [CallerMemberName] string callerName = "", [CallerLineNumber] int lineNumber = 0) {
             //Read
-            Settings.Default.Debug += $"[{DateTime.Now}] {text}\n";
+            Settings.Default.Debug += $"[{DateTime.Now}][method:{callerName} line:{lineNumber}] {text}\n";
             Log(text);
         }
 
-        private static void Log(string text) {
+        private static void Log(string text, [CallerMemberName] string callerName = "", [CallerLineNumber] int lineNumber = 0) {
             if (!Directory.Exists("logs"))
                 Directory.CreateDirectory("logs");
             try {
-                File.AppendAllText($"logs/{DateTime.Now.ToShortDateString()}-mc.log", $"[{DateTime.Now}] {text}\n");
+                File.AppendAllText($"logs/{DateTime.Now.ToShortDateString()}-mc.log", $"[{DateTime.Now}][method:{callerName} line:{lineNumber}] {text}\n");
             }
             catch {
                 // ignored
