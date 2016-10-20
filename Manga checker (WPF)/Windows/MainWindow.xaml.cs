806528db3c8dba7791b337f6b39323fb2d1c9d81
tensei <davuttensei@gmail.com>
2016-10-20 07:04:05 +0200

    little design change

--- Manga checker (WPF)/Windows/MainWindow.xaml.cs
+++ Manga checker (WPF)/Windows/MainWindow.xaml.cs
@@ -8,36 +8,40 @@ using MangaChecker.Properties;
 using MangaChecker.ViewModels;
 
 namespace MangaChecker.Windows {
-    /// <summary>
-    ///     Interaktionslogik für MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow {
-        public MainWindow() {
-            InitializeComponent();
-            Settings.Default.Debug = "Debug shit goes in here!\n";
-        }
+	/// <summary>
+	///     Interaktionslogik für MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow {
+		public MainWindow() {
+			InitializeComponent();
+			Settings.Default.Debug = "Debug shit goes in here!\n";
+		}
 
-        private void DebugTextBox_TextChanged(object sender, TextChangedEventArgs e) {
-            DebugTextBox.ScrollToEnd();
-        }
+		private void DebugTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+			DebugTextBox.ScrollToEnd();
+		}
 
-        private void Grid_Loaded(object sender, RoutedEventArgs e) {
-            // ButtonColorChange();
-            if (!File.Exists("MangaDB.sqlite")) {
-                Tools.CreateDb();
-            }
-        }
+		private void Grid_Loaded(object sender, RoutedEventArgs e) {
+			// ButtonColorChange();
+			if (!File.Exists("MangaDB.sqlite")) {
+				Tools.CreateDb();
+			}
+		}
 
-        private void Grid_MouseDown(object sender, MouseButtonEventArgs e) {
-            if (e.ChangedButton == MouseButton.Left) DragMove();
-        }
-        
-        private void TopMostBtn_Click(object sender, RoutedEventArgs e) {
-            Topmost = Topmost == false;
-        }
+		private void Grid_MouseDown(object sender, MouseButtonEventArgs e) {
+			if (e.ChangedButton == MouseButton.Left) DragMove();
+		}
 
-        private void MainWindow_OnClosed(object sender, EventArgs e) {
-            MainWindowViewModel.Instance.ChildThread.Abort();
-        }
-    }
+		private void TopMostBtn_Click(object sender, RoutedEventArgs e) {
+			Topmost = Topmost == false;
+		}
+
+		private void MainWindow_OnClosed(object sender, EventArgs e) {
+			try {
+				MainWindowViewModel.Instance.ChildThread.Abort();
+			} catch (Exception) {
+				// ignored
+			}
+		}
+	}
 }
